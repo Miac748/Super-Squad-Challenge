b91b3310aeaaf907164bbc0adf20ec747083739b
Mia Cuevas <mcueva174@west-mec.org>
2024-09-26 14:11:47 -0700

    9/26/24

--- server/index.js
+++ server/index.js
@@ -81,27 +81,28 @@ app.put('/update-user/:currentName/:currentEmail', async (req, res) => {
     try {
         const { currentName, currentEmail } = req.params;
         const { newName, newEmail } = req.body;
-        console.log('Current user:', { currentName, currentEmail });
-        console.log('New user data:', { newName, newEmail });
+        console.log('Current user:', { currentName, currentPower });
+        console.log('New user data:', { newName, newPower });
         const data = await fs.readFile(dataPath, 'utf8');
         if (data) {
             let users = JSON.parse(data);
-            const userIndex = users.findIndex(user => user.name === currentName && user.email === currentEmail);
+            const userIndex = users.findIndex(user => user.name === currentName && user.message === currentPower);
             console.log(userIndex);
             if (userIndex === -1) {
                 return res.status(404).json({ message: "User not found" })
             }
-            users[userIndex] = { ...users[userIndex], name: newName, email: newEmail };
+            users[userIndex] = { ...users[userIndex], name: newName, email: newPower };
             console.log(users);
             await fs.writeFile(dataPath, JSON.stringify(users, null, 2));
 
-            res.status(200).json({ message: `You sent ${newName} and ${newEmail}` });
+            res.status(200).json({ message: `You sent ${newName} and ${newPower}` });
         }
     } catch (error) {
         console.error('Error updating user:', error);
         res.status(500).send('An error occurred while updating the user.');
     }
 });
+
 
 // Start the server
 const PORT = process.env.PORT || 3000;
